2625ce06ae81188da1a6d27f98c92ca3f943b707
Abby Bahlmann <abbyb777@gmail.com>
2019-08-01 21:44:15 -0400

    buttons and objects and ajax

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,24 @@
+var foodtruckResponse;
+var weatherResponse;
+
+$.ajax({
+ url: "https://data.boston.gov/api/3/action/datastore_search?resource_id=9960513f-1a7f-4f02-847c-553e7c8a60f7",
+ method: "GET"
+}).then(function(response) {
+ document.write(JSON.stringify(response));
+});
+
+
+$.ajax({
+    url: "https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=74694f219dec1ad3f001b89fcece53c7",
+    method: "GET"
+   }).then(function(response) {
+    weatherResponse=response;
+    console.log(response);
+    console.log(response.Runtime);
+   });
+
+
 var centralCambridge={
     city:"Cambridge",
     clouds:"",
@@ -22,6 +43,8 @@ function newPage(){
 }
 
 function findFoodTrucks(location){
+
+
     for (var i=0;i<records.length;i++){
     if ((location.xCoordinate+.001===records[i].x||location.yCoordinate-.001===records[i].y))
     {
@@ -35,14 +58,14 @@ function findFoodTrucks(location){
 }
 
 function findWeather(location){
-    location.city
+
 }
 
 
 $("#central-cambridge").on("click", function(){
     newPage();
     findFoodTrucks(centralCambridge);
-    findWeather(location);
+    findWeather(centralCambridge);
     for (var i=0; i<foodTruckNames.length;i++){
         $()
     }
