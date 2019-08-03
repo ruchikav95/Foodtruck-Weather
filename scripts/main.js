3bf6fe03dcba4a1d089979a57b099e9b12c73e0c
Abby Bahlmann <abbyb777@gmail.com>
2019-08-03 15:17:34 -0400

    help

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,38 @@
 
-$.ajax({
-  url:
-    "https://data.boston.gov/api/3/action/datastore_search?resource_id=9960513f-1a7f-4f02-847c-553e7c8a60f7",
-  method: "GET"
-}).then(function(response) {
-  document.write(JSON.stringify(response));
-});
+// $.ajax({
+//   url:
+//     "https://data.boston.gov/api/3/action/datastore_search?resource_id=9960513f-1a7f-4f02-847c-553e7c8a60f7",
+//   method: "GET"
+// }).then(function(response) {
+//   document.write(JSON.stringify(response));
+// });
 
-$.ajax({
-  url:
-    "https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=74694f219dec1ad3f001b89fcece53c7",
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-  console.log(response.Runtime);
-});
-var foodtruckResponse;
-var weatherResponse;
-
-$.ajax({
- url: "https://data.boston.gov/api/3/action/datastore_search?resource_id=9960513f-1a7f-4f02-847c-553e7c8a60f7",
- method: "GET"
-}).then(function(response) {
- document.write(JSON.stringify(response));
-});
+// $.ajax({
+//   url:
+//     "https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=74694f219dec1ad3f001b89fcece53c7",
+//   method: "GET"
+// }).then(function(response) {
+//   console.log(response);
+//   console.log(response.Runtime);
+// });
+// var foodtruckResponse;
+// var weatherResponse;
 
 
-$.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=74694f219dec1ad3f001b89fcece53c7",
-    method: "GET"
-   }).then(function(response) {
-    weatherResponse=response;
-    console.log(response);
-    console.log(response.Runtime);
-   });
+
+
+// $.ajax({
+//     url: "https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=74694f219dec1ad3f001b89fcece53c7",
+//     method: "GET"
+//    }).then(function(response) {
+//     weatherResponse=response;
+//     console.log(response);
+//     console.log(response.Runtime);
+//    });
 
 
 var centralCambridge={
+    name: "Central Square",
     city:"Cambridge",
     clouds:"",
     precipitation:"",
@@ -52,18 +48,24 @@ var centralCambridge={
     foodTruckPinpoint:[]
 }
 
-var records=foodTruckResponse.result.records;
 
-function newPage(){
-    $("#search-screen").hide();
-    $("#display-screen").show();
-}
+var records;
 
 function findFoodTrucks(location){
 
+    console.log(location.name);
+    $.ajax({
+        url: "https://data.boston.gov/api/3/action/datastore_search?resource_id=9960513f-1a7f-4f02-847c-553e7c8a60f7",
+        dataType:'jsonp',
+        method: "GET",
+       }).then(function(response) {
+        document.write(JSON.stringify(response));
+        records=response.result.records;
+        console.log(response);
+       });
 
     for (var i=0;i<records.length;i++){
-    if ((location.xCoordinate+.001===records[i].x||location.yCoordinate-.001===records[i].y))
+    if ((location.xCoordinate+.001===records[i].x||location.xCoordinate-.001===records[i].x)&&(location.yCoordinate+.001===records[i].y||location.yCoordinate-.001===records[i].y))
     {
         location.foodTruckLocations.push(records.Location);
         location.foodTruckNames.push(records.Truck);
@@ -77,12 +79,21 @@ function findFoodTrucks(location){
 function findWeather(location){
 
 }
+console.log(centralCambridge.name);
 
+function openDisplay(location){
+    $("#search-screen").hide();
+    $("#display-screen").show();
+    $("#display-header").text(location.name + ", "+location.city);
+    findFoodTrucks(location);
+    // findWeather(location);
+
+}
 
 $("#central-cambridge").on("click", function(){
-    newPage();
-    findFoodTrucks(centralCambridge);
-    findWeather(centralCambridge);
-    for (var i=0; i<foodTruckNames.length;i++){
-        $()
+    console.log(centralCambridge.name);
+    openDisplay(centralCambridge);
+    for (var i=0; i<centralCambridge.foodTruckNames.length;i++){
+        $("#food-trucks").after("<p>"+centralCambridge.foodTruckNames[i]+"</p>")
     }
+});
